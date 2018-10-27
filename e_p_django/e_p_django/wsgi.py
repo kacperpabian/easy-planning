1e872b0ca4b0cde7a7e5cc3683459f8c193a2dd4
Student226123 <Kacper2345!>
2018-10-27 23:50:27 +0200

    project initialization

---
+++ e_p_django/e_p_django/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for e_p_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os, sys
+
+from django.core.wsgi import get_wsgi_application
+
+path = '/Users/Student226123/Desktop/inzynierka/easy-planning/e_p_django'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_p_django.settings')
+
+application = get_wsgi_application()
