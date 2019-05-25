ff967faacf8c5889333b85f217075a744ce98c90
Student226123 <Kacper2345!>
2019-05-25 22:32:09 +0200

    3.6

--- e_p_django/e_p_django/wsgi.py
+++ e_p_django/e_p_django/wsgi.py
@@ -5,15 +5,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os, sys
+import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-path = '/Users/Student226123/Desktop/inzynierka/easy-planning/e_p_django'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_p_django.e_p_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', './settings')
 
 application = DjangoWhiteNoise(get_wsgi_application())
