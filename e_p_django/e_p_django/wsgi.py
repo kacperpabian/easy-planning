35f22e730194bd4b14d2ef70c7ad022eec3f4750
Student226123 <Kacper2345!>
2019-06-09 19:49:52 +0200

    wsgi

--- e_p_django/e_p_django/wsgi.py
+++ e_p_django/e_p_django/wsgi.py
@@ -10,6 +10,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "e_p_django.e_p_django.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "e_p_django.settings"
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
